03f9417846cda4c0d028952bc6e056cf93895e16
Jonas Signoreti <64449236+jonassignoreti@users.noreply.github.com>
2020-10-12 22:40:46 -0300

    updating

---
+++ aula13/tests.js
@@ -0,0 +1,19 @@
+/*
+var fatiapizza = 8
+
+while (fatiapizza != 0){
+    console.log(`${fatiapizza} fatia(s)`)
+    fatiapizza --
+}
+
+console.log('Pizza FINALIZADA')
+*/
+
+var fatiapizza = 8
+
+do{
+    console.log(`${fatiapizza} fatia(s)`)
+    fatiapizza --
+} while (fatiapizza != 0)
+
+console.log('Pizza FINALIZADA')
